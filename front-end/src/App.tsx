f6dfe468620f5e44ed93d69cdab75684b8548c05
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-14 03:01:06 -0300

    feat: adiciona DrawerContext

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/Router';
-import { AppThemeProvider } from './shared/contexts';
-import { Sidebar } from './shared/components/Sidebar/Sidebar';
+import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 
 export default function App() {
     return (
         <AppThemeProvider>
-                <RouterProvider router={router} />                
+            <DrawerProvider>
+                <RouterProvider router={router} />                  
+            </DrawerProvider>
         </AppThemeProvider>
     );
 }
