4781c0dcdbe30af8136cc6003b80c8b2692cc1d2
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-14 15:05:39 -0300

    feat: adiciona responsividade no layout base

--- front-end/src/shared/layouts/BasePageLayout.tsx
+++ front-end/src/shared/layouts/BasePageLayout.tsx
@@ -15,20 +15,39 @@ export const BasePageLayout: React.FC<IBasePageLayoutProps> = ({
     barraDeFerramentas,
 }) => {
     const theme = useTheme();
+
     const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const mdDown = useMediaQuery(theme.breakpoints.down('md'));
+
     const { toggleDrawerOpen } = useDrawerContext();
+
     return (
         <Box height="100%" display="flex" flexDirection="column" gap={1}>
-            <Box padding={1} display="flex" alignItems="center" height={theme.spacing(12)} gap={1}>
+            <Box
+                padding={1}
+                display="flex"
+                alignItems="center"
+                gap={1}
+                height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
+            >
                 {smDown && (
                     <IconButton onClick={toggleDrawerOpen}>
                         <MenuIcon />
                     </IconButton>
                 )}
-                <Typography variant="h5">{title}</Typography>
+                <Typography
+                    variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+                    overflow="hidden"
+                    whiteSpace="nowrap"
+                    textOverflow="ellipsis"
+                >
+                    {title}
+                </Typography>
             </Box>
             {barraDeFerramentas && <Box>{barraDeFerramentas}</Box>}
-            <Box>{children}</Box>
+            <Box flex={1} overflow="auto">
+                {children}
+            </Box>
         </Box>
     );
 };
