1c97c84c6d7252278790666b6e22b54a6d93c346
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-14 14:33:11 -0300

    feat: adiciona layoute base

---
+++ front-end/src/shared/layouts/BasePageLayout.tsx
@@ -0,0 +1,27 @@
+import { Box, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material';
+import type { ReactNode } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useDrawerContext } from '../contexts';
+interface IBasePageLayoutProps {
+    children: ReactNode;
+    title: string;
+}
+export const BasePageLayout: React.FC<IBasePageLayoutProps> = ({ children, title }) => {
+    const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const { toggleDrawerOpen } = useDrawerContext();
+    return (
+        <Box height="100%" display="flex" flexDirection="column" gap={1}>
+            <Box padding={1} display="flex" alignItems="center" height={theme.spacing(12)} gap={1}>
+                {smDown && (
+                    <IconButton onClick={toggleDrawerOpen}>
+                        <MenuIcon />
+                    </IconButton>
+                )}
+                <Typography variant="h5">{title}</Typography>
+            </Box>
+            <Box>Barra de ferramentas</Box>
+            <Box>{children}</Box>
+        </Box>
+    );
+};
