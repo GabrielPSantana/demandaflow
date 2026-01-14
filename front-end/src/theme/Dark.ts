abf677f9f97fdbe52d617802be65a37437a9e8ad
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-13 22:59:41 -0300

    feat: adiciona tema dark

---
+++ front-end/src/theme/Dark.ts
@@ -0,0 +1,25 @@
+import { createTheme } from "@mui/material";
+import { cyan, red } from "@mui/material/colors";
+
+export const DarkTheme = createTheme({
+    palette: {
+        primary: {
+            main: red[700],
+            dark: red[800],
+            light: red[500],
+            contrastText: '#FFF',
+        },
+
+        secondary: {
+            main: cyan[500],
+            dark: cyan[400],
+            light: cyan[300],
+            contrastText: '#FFF',
+        },
+
+        background: {
+            default: '#303134',
+            paper: '#202124',
+        }
+    }
+})
