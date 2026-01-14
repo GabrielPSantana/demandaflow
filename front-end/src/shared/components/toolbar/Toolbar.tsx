9dd6dcfca51f777262617175fbadf5fa9be58444
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-14 17:11:19 -0300

    feat: adiciona componente de barra de ferramentas

---
+++ front-end/src/shared/components/toolbar/Toolbar.tsx
@@ -0,0 +1,57 @@
+import { Box, Button, Paper, TextField, useTheme } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
+interface IToolbarPros {
+    searchText?: string;
+    showSearchInput?: boolean;
+    onSearchTextChange?: (newText: string) => void;
+
+    newButtonText?: string;
+    showNewButton?: boolean;
+    onClickNewButton?: () => void;
+}
+
+export const Toolbar = ({
+    searchText = '',
+    showSearchInput = false,
+    onSearchTextChange,
+    newButtonText = 'Novo',
+    showNewButton = true,
+    onClickNewButton,
+}: IToolbarPros) => {
+    const theme = useTheme();
+    return (
+        <Box
+            display="flex"
+            gap={1}
+            marginX={1}
+            padding={1}
+            paddingX={2}
+            alignItems="center"
+            height={theme.spacing(6)}
+            component={Paper}
+        >
+            {showSearchInput && (
+                <TextField
+                    value={searchText}
+                    onChange={(event) => onSearchTextChange?.(event.target.value)}
+                    size="small"
+                    placeholder="Pesquisar..."
+                />
+            )}
+            <Box display="flex" justifyContent="end" flex={1}>
+                {showNewButton && (
+                    <Button
+                        onClick={onClickNewButton}
+                        variant="contained"
+                        color="primary"
+                        disableElevation
+                        endIcon={<AddIcon />}
+                    >
+                        {newButtonText}
+                    </Button>
+                )}
+            </Box>
+        </Box>
+    );
+};
