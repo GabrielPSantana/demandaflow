201dc8388c1ccc6de1f257e3e35171c98ec7e979
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-15 00:42:11 -0300

    fix: altera diretório da página de tarefas

--- front-end/src/pages/tasks/Tasks.tsx
+++ front-end/src/pages/tasks/Tasks.tsx
@@ -1,10 +1,12 @@
+import { DetailToolbar } from '../../shared/components/DetailToolbar/DetailToolbar';
 import { useDrawerContext } from '../../shared/contexts';
 import { BasePageLayout } from '../../shared/layouts';
 
 export default function Tasks() {
     const { toggleDrawerOpen } = useDrawerContext();
+    
     return (
-        <BasePageLayout title="Atividades" barraDeFerramentas={<>barraDeFerramentas</>}>
+        <BasePageLayout title="Atividades" toolbar={<DetailToolbar showLoadingSaveAndCloseButton={true}/>}>
             Testando task
         </BasePageLayout>
     );
