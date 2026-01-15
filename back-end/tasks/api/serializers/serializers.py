60af0ac75b7950b0a078857f8673850304614706
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-15 03:15:23 -0300

    feat: adiciona serializer

---
+++ back-end/tasks/api/serializers/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from ..models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+
+        fields = [
+            'title', 'description', 'priority', 
+            'status', 'start_datetime', 'end_datetime',
+            'time_spent', 'team_id'
+        ]
+
+    
