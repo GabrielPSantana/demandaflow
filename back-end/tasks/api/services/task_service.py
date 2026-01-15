c849ed8f0873228c1af9a5af70efee396e1d7a29
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-15 15:21:58 -0300

    feat: adiciona classe abstrata de serviço de tarefas

--- back-end/tasks/api/services/task_service.py
+++ back-end/tasks/api/services/task_service.py
@@ -1,26 +1,6 @@
-from django.shortcuts import get_object_or_404
-from api.models import Task
+from abc import ABC, abstractmethod
 
-class TaskService:
-    @staticmethod
-    def list_tasks():
-        return Task.objects.all()
-
-    @staticmethod
-    def get_task(tasks_id):
-        return get_object_or_404(Task, tasks_id=tasks_id)
-
-    @staticmethod
-    def create_task(data):
-        return Task.objects.create(**data)
-
-    @staticmethod
-    def update_task(task, data):
-        for field, value in data.items():
-            setattr(task, field, value)
-        task.save()
-        return task
-
-    @staticmethod
-    def delete_task(task):
-        task.delete()
+class TaskService(ABC):
+    @abstractmethod
+    def execute(self, **kwargs):
+        pass
