1a1164a622afadabaff8af9ea691c9c8c632ccf1
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-15 04:19:04 -0300

    feat: adiciona view list_tasks

--- back-end/tasks/api/views.py
+++ back-end/tasks/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Task
+from .serializers.serializers import TaskSerializer
+
+@api_view(['GET'])
+def list_tasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
