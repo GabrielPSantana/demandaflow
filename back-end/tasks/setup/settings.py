e1bf25c9bae80674994dab30a0ea389123f638c7
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-15 21:29:25 -0300

    chore: renomea app de config

--- back-end/tasks/setup/settings.py
+++ back-end/tasks/setup/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'tasks.urls'
+ROOT_URLCONF = 'setup.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'tasks.wsgi.application'
+WSGI_APPLICATION = 'setup.wsgi.application'
 
 
 # Database
