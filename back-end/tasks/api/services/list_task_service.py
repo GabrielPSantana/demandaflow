defcaa7284d076825c431b5700d9e857504028be
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-16 01:40:33 -0300

    feat: adiciona list task service

--- back-end/tasks/api/services/list_task_service.py
+++ back-end/tasks/api/services/list_task_service.py
@@ -2,12 +2,16 @@ from api.models import Task
 
 class ListTaskService:
     def execute(self, user_id, team_id=None, is_manager=False):
-        repo = Task.objects
+        try:
+            task_repository = Task.objects
 
-        if is_manager and team_id:
-            return repo.by_team(team_id)
+            if is_manager and team_id:
+                return task_repository.by_team(team_id)
 
-        if team_id:
-            return repo.by_user_in_team(user_id, team_id)
+            if team_id:
+                return task_repository.by_user_in_team(user_id, team_id)
 
-        return repo.by_user_without_team(user_id)
+            return task_repository.by_user_without_team(user_id)
+
+        except Exception as e:
+            raise Exception("Erro ao listar tasks.")
