85df13f11c101740ea4d3950c5fef5eb1964d6aa
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-15 21:19:33 -0300

    feat: adiciona rota de listagem da api

---
+++ back-end/tasks/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from api import views
+from api.views import TaskViewSet
+
+urlpatterns = [
+    path('tasks/', TaskViewSet.as_view({'get': 'list'}), name='task-list'),
+]
