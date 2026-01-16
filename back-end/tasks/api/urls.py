7d4f053b36084919c388d665dcecf66d65f8978e
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-16 17:38:12 -0300

    feat: adiciona url delete e otimiza importação

--- back-end/tasks/api/urls.py
+++ back-end/tasks/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import TaskListViewSet
-from .views import TaskCreateViewSet
-from .views import TaskUpdateViewSet
+from .views import TaskListViewSet, TaskCreateViewSet, TaskUpdateViewSet, TaskDeleteViewSet
 
 urlpatterns = [
     path('tasks/', TaskListViewSet.as_view({'get': 'list'}), name='task-list'),
     path('tasks/create/', TaskCreateViewSet.as_view({'post': 'create'}), name='task-create'),
-    path('tasks/update/', TaskUpdateViewSet.as_view({'put': 'update'}), name='task-create'),
+    path('tasks/update/', TaskUpdateViewSet.as_view({'put': 'update'}), name='task-update'),
+    path('tasks/delete/', TaskDeleteViewSet.as_view({'delete': 'destroy'}), name='task-destroy'),
 ]
