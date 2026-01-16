9fe973f0aaa691f781bf1f2c3cb2f894214bd3d1
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-16 01:35:27 -0300

    feat: configura list serializer

--- back-end/tasks/api/serializers/task_list_serializer.py
+++ back-end/tasks/api/serializers/task_list_serializer.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 class TaskListSerializer(serializers.Serializer):
     user_id = serializers.UUIDField(required=True)
     team_id = serializers.UUIDField(required=False)
-    is_manager = serializers.BooleanField(required=False, default=False)
+    is_manager = serializers.BooleanField(required=False)
