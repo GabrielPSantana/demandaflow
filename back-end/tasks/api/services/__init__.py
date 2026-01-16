3ea027111a0a3a74d80510196c5834b399264f98
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-16 01:14:44 -0300

    feat: adiciona create task service

--- back-end/tasks/api/services/__init__.py
+++ back-end/tasks/api/services/__init__.py
@@ -1,1 +1,2 @@
 from .list_task_service import ListTaskService
+from .create_task_service import CreateTaskService
---
+++ back-end/tasks/api/services/create_task_service.py
@@ -0,0 +1,49 @@
+from api.models import Task
+from datetime import datetime
+from django.core.exceptions import ValidationError
+
+class CreateTaskService:
+    def execute(
+        self,
+        user_id,
+        title,
+        description=None,
+        team_id=None,
+        priority='LOW',
+        status='PENDING',
+        start_datetime=None,
+        end_datetime=None,
+        time_spent=None,
+        published=True
+    ):
+        try:
+            if start_datetime and end_datetime:
+                try:
+                    start_str = start_datetime
+                    end_str = end_datetime
+
+                    start_datetime = datetime.fromisoformat(start_str.replace("Z", "+00:00"))
+                    end_datetime = datetime.fromisoformat(end_str.replace("Z", "+00:00"))
+
+                except ValueError as e:
+                    raise ValidationError(f"Formato inválido de data/hora: {e}")
+
+                time_spent = end_datetime - start_datetime
+
+            task = Task.objects.create(
+                user_id=user_id,
+                title=title,
+                description=description,
+                team_id=team_id,
+                priority=priority,
+                status=status,
+                start_datetime=start_datetime,
+                end_datetime=end_datetime,
+                time_spent=time_spent,
+                published=published
+            )
+
+            return task
+
+        except Exception as e:
+            raise ValidationError(f"Ocorreu um erro ao criar a task: {e}")
