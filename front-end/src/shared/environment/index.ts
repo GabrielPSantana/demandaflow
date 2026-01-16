a05cb0e514498ecaf246dc3606012a5f51255ae8
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-16 20:58:53 -0300

    feat: adiciona Environment

---
+++ front-end/src/shared/environment/index.ts
@@ -0,0 +1,21 @@
+export const Environment = {
+    /**
+     * Define a quantidade de linhas a ser carregada por padrão nas listagens
+     */
+    LINE_LIMIT: 10,
+
+    /**
+     * Placeholder exibido nos inputs de busca
+     */
+    SEARCH_INPUT_PLACEHOLDER: 'Pesquisar...',
+
+    /**
+     * Texto exibido quando nenhum registro é encontrado em uma listagem
+     */
+    EMPTY_LISTING: 'Nenhum registro encontrado',
+
+    /**
+     * URL base para consultas à API
+     */
+    BASE_URL: 'http://localhost:3333',
+};
