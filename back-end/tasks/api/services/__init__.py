c4f2a6df160d96496c643177c808dd6026fb87b1
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-16 17:36:44 -0300

    feat: adiciona importação do delete service

--- back-end/tasks/api/services/__init__.py
+++ back-end/tasks/api/services/__init__.py
@@ -1,3 +1,4 @@
 from .task_list_service import TaskListService
 from .task_create_service import TaskCreateService
 from .task_update_service import TaskUpdateService
+from .task_delete_service import TaskDeleteService
