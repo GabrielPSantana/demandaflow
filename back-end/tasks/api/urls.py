043bce2c55aaba556504ee6e7d3327a2cff1878d
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-16 01:16:15 -0300

    feat: adiciona path task/create/

--- back-end/tasks/api/urls.py
+++ back-end/tasks/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from api.views import TaskListViewSet
+from .views import TaskListViewSet
+from .views import TaskCreateViewSet
 
 urlpatterns = [
     path('tasks/', TaskListViewSet.as_view({'get': 'list'}), name='task-list'),
-    
+    path('tasks/create/', TaskCreateViewSet.as_view({'post': 'create'}), name='task-create'),
 ]
