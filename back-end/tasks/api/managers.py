59c1d254162910144073340d63e9b2971b422242
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-15 21:08:46 -0300

    feat: adiciona task manager

--- back-end/tasks/api/managers.py
+++ back-end/tasks/api/managers.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 class TaskQuerySet(models.QuerySet):
-    def published(self):
-        return self.filter(published=True)
+    def by_team(self, team_id):
+        return self.filter(team_id=team_id)
+
+    def by_user_in_team(self, user_id, team_id):
+        return self.filter(user_id=user_id, team_id=team_id)
+
+    def by_user_without_team(self, user_id):
+        return self.filter(user_id=user_id, team_id__isnull=True)
 
 class TaskManager(models.Manager):
     def get_queryset(self):
         return TaskQuerySet(self.model, using=self._db)
+
+    def by_team(self, team_id):
+        return self.get_queryset().by_team(team_id)
+
+    def by_user_in_team(self, user_id, team_id):
+        return self.get_queryset().by_user_in_team(user_id, team_id)
+
+    def by_user_without_team(self, user_id):
+        return self.get_queryset().by_user_without_team(user_id)
