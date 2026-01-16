4215b9befeb98340b2bbff21f4e547168de873ce
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-15 21:13:11 -0300

    feat: renomeado task serializer

--- back-end/tasks/api/serializers/task_serializer.py
+++ back-end/tasks/api/serializers/task_serializer.py
@@ -4,11 +4,9 @@ from ..models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-
         fields = [
-            'tasks_id','title', 'description', 'priority', 
+            'task_id', 'title', 'description', 'priority',
             'status', 'start_datetime', 'end_datetime',
             'time_spent', 'team_id'
         ]
-
-    
+ 
