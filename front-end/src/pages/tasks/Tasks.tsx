5b285cba1c6c745a9cffbcc2662df842c3ea9bef
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-17 06:24:37 -0300

    feat: adiciona useDebounce

--- front-end/src/pages/tasks/Tasks.tsx
+++ front-end/src/pages/tasks/Tasks.tsx
@@ -1,14 +1,30 @@
-import {  useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { ListToolbar } from '../../shared/components/ListToolbar/ListToolbar';
 import { BasePageLayout } from '../../shared/layouts';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
+import { TasksService } from '../../shared/services/api/tasks/TasksService';
+import { useDebounce } from '../../shared/hooks';
 
 export default function Tasks() {
     const [searchParams, setSearchParams] = useSearchParams();
+    const { debounce } = useDebounce()
 
     const search = useMemo(() => {
         return searchParams.get('search') || '';
     }, [searchParams]);
+
+    useEffect(() => {
+        debounce(()=> {
+            TasksService.getAll()
+                .then((result) => {
+                    if(result instanceof Error){
+                        alert(result.message)
+                    }
+                    console.log(result)
+                });            
+        })
+
+    }, [search]);
 
     return (
         <BasePageLayout
