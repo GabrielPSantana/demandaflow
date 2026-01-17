771e825688e94d28696e4650a7dad48d6a27efc8
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-17 03:07:12 -0300

    feat: adiciona lista de ferramenta no tasks page

--- front-end/src/pages/tasks/Tasks.tsx
+++ front-end/src/pages/tasks/Tasks.tsx
@@ -1,12 +1,29 @@
-import { DetailToolbar } from '../../shared/components/DetailToolbar/DetailToolbar';
-import { useDrawerContext } from '../../shared/contexts';
+import {  useSearchParams } from 'react-router-dom';
+import { ListToolbar } from '../../shared/components/ListToolbar/ListToolbar';
 import { BasePageLayout } from '../../shared/layouts';
+import { useMemo } from 'react';
 
 export default function Tasks() {
-    const { toggleDrawerOpen } = useDrawerContext();
-    
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const search = useMemo(() => {
+        return searchParams.get('search') || '';
+    }, [searchParams]);
+
     return (
-        <BasePageLayout title="Atividades" toolbar={<DetailToolbar showLoadingSaveAndCloseButton={true}/>}>
+        <BasePageLayout
+            title="Atividades"
+            toolbar={
+                <ListToolbar
+                    showSearchInput
+                    searchText={search}
+                    newButtonText="Nova"
+                    onSearchTextChange={(text) =>
+                        setSearchParams({ search: text }, { replace: true })
+                    }
+                />
+            }
+        >
             Testando task
         </BasePageLayout>
     );
