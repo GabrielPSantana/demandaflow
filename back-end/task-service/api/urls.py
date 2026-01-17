bfb61e1e99e1587e7eeff10bed994a5517e3d60f
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-17 19:36:19 -0300

    feat: adiciona parametros de rotas para update e delete

--- back-end/task-service/api/urls.py
+++ back-end/task-service/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskListViewSet, TaskCreateViewSet, TaskUpdateViewSet, TaskDeleteViewSet
+from .views import TaskListViewSet, TaskCreateViewSet,TaskReadViewSet, TaskUpdateViewSet, TaskDeleteViewSet
 
 urlpatterns = [
     path('tasks/', TaskListViewSet.as_view({'get': 'list'}), name='task-list'),
+    path('tasks/<uuid:task_id>/', TaskReadViewSet.as_view({'get': 'retrieve'}), name='task-detail'),
     path('tasks/create/', TaskCreateViewSet.as_view({'post': 'create'}), name='task-create'),
-    path('tasks/update/', TaskUpdateViewSet.as_view({'put': 'update'}), name='task-update'),
-    path('tasks/delete/', TaskDeleteViewSet.as_view({'delete': 'destroy'}), name='task-destroy'),
+    path('tasks/update/<uuid:task_id>/', TaskUpdateViewSet.as_view({'put': 'update'}), name='task-update'),
+    path('tasks/delete/<uuid:task_id>/', TaskDeleteViewSet.as_view({'delete': 'destroy'}), name='task-destroy'),
 ]
