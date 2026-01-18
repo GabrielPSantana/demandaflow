a4e812f42f3f8cd663d6f52f1d0f5f76ef2081f0
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-18 04:44:38 -0300

    feat: adiciona a importção do service de lida de uma atividade

--- back-end/task-service/api/services/__init__.py
+++ back-end/task-service/api/services/__init__.py
@@ -2,3 +2,4 @@ from .task_list_service import TaskListService
 from .task_create_service import TaskCreateService
 from .task_update_service import TaskUpdateService
 from .task_delete_service import TaskDeleteService
+from .task_read_service import TaskReadService  
