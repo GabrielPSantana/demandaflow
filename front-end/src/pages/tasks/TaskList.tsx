563f5e5e3c0dee86460b4010a83358b3e73ad63b
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-18 01:30:59 -0300

    feat: adiciona listagem de tarefas

--- front-end/src/pages/tasks/TaskList.tsx
+++ front-end/src/pages/tasks/TaskList.tsx
@@ -1,28 +1,77 @@
 import { useSearchParams } from 'react-router-dom';
 import { ListToolbar } from '../../shared/components/ListToolbar/ListToolbar';
 import { BasePageLayout } from '../../shared/layouts';
-import { useEffect, useMemo } from 'react';
-import { TasksService } from '../../shared/services/api/tasks/TasksService';
+import { useEffect, useMemo, useState } from 'react';
+import { TasksService, type ITaskList } from '../../shared/services/api/tasks/TasksService';
 import { useDebounce } from '../../shared/hooks';
+import {
+    LinearProgress,
+    Pagination,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TableHead,
+    TableRow,
+} from '@mui/material';
+import { Environment } from '../../shared/environment';
+
+type Priority = 'CRITICAL' | 'HIGH' | 'MEDIUM' | 'LOW';
+type Status = 'PENDING' | 'IN_PROGRESS' | 'COMPLETED';
 
 export default function TasksList() {
     const [searchParams, setSearchParams] = useSearchParams();
     const { debounce } = useDebounce();
+
+    const [rowsTasks, setRowTaks] = useState<ITaskList[]>([]);
+    const [totalCountTasks, setTotalCountTaks] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const priorityColorMap: Record<Priority, string> = {
+        CRITICAL: 'error.main',
+        HIGH: 'warning.main',
+        MEDIUM: 'info.main',
+        LOW: 'success.main',
+    };
+
+    const statusLabelMap: Record<Status, string> = {
+        PENDING: 'Pedente',
+        IN_PROGRESS: 'Em progresso',
+        COMPLETED: 'Completo',
+    };
+
+    const priorityLabelMap: Record<Priority, string> = {
+        CRITICAL: 'Crítico',
+        HIGH: 'Alto',
+        MEDIUM: 'Médio',
+        LOW: 'Baixo',
+    };
+
+    const page = useMemo(() => {
+        return Number(searchParams.get('page') || '1');
+    }, [searchParams]);
 
     const search = useMemo(() => {
         return searchParams.get('search') || '';
     }, [searchParams]);
 
     useEffect(() => {
+        setIsLoading(true);
+
         debounce(() => {
-            TasksService.getAll().then((result) => {
+            TasksService.getAll(search, page).then((result) => {
+                setIsLoading(false);
                 if (result instanceof Error) {
-                    alert(result.message);
+                    return;
                 }
-                console.log(result);
+
+                setRowTaks(result.data);
+                setTotalCountTaks(result.totalCount);
             });
         });
-    }, [search]);
+    }, [search, page]);
 
     return (
         <BasePageLayout
@@ -33,12 +82,86 @@ export default function TasksList() {
                     searchText={search}
                     newButtonText="Nova"
                     onSearchTextChange={(text) =>
-                        setSearchParams({ search: text }, { replace: true })
+                        setSearchParams({ page: '1', search: text }, { replace: true })
                     }
                 />
             }
         >
-            Testando task
+            <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
+                <Table>
+                    <TableHead>
+                        <TableRow >
+                            <TableCell sx={{fontWeight: 'bold'}}>Título</TableCell>
+                            <TableCell sx={{fontWeight: 'bold'}}>Prioridade</TableCell>
+                            <TableCell sx={{fontWeight: 'bold'}}>Status</TableCell>
+                            <TableCell sx={{fontWeight: 'bold'}}>Data de início</TableCell>
+                            <TableCell sx={{fontWeight: 'bold'}}>Data de término</TableCell>
+                            <TableCell sx={{fontWeight: 'bold'}}>Tempo gasto</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {rowsTasks.map((row) => (
+                            <TableRow key={row.task_id} sx={{ margin: 50 }}>
+                                <TableCell>{row.title}</TableCell>
+
+                                <TableCell
+                                    sx={{
+                                        color: priorityColorMap[row.priority as Priority],
+                                        fontWeight: 'bold',
+                                        borderRadius: 4,
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    {priorityLabelMap[row.priority as Priority]}
+                                </TableCell>
+
+                                <TableCell>{statusLabelMap[row.status as Status]}</TableCell>
+
+                                <TableCell>
+                                    {new Date(row.start_datetime).toLocaleString()}
+                                </TableCell>
+
+                                <TableCell>{new Date(row.end_datetime).toLocaleString()}</TableCell>
+
+                                <TableCell>{row.time_spent}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+
+                    {totalCountTasks === 0 && !isLoading && (
+                        <caption>{Environment.EMPTY_LISTING}</caption>
+                    )}
+
+                    <TableFooter>
+                        {isLoading && (
+                            <TableRow>
+                                <TableCell colSpan={6}>
+                                    <LinearProgress variant="indeterminate" />
+                                </TableCell>
+                            </TableRow>
+                        )}
+
+                        {totalCountTasks > 0 && totalCountTasks > Environment.LINE_LIMIT && (
+                            <TableRow>
+                                <TableCell colSpan={6}>
+                                    <Pagination
+                                        page={page}
+                                        count={totalCountTasks}
+                                        shape="rounded"
+                                        onChange={(_, newPage) =>
+                                            setSearchParams(
+                                                { page: newPage.toString(), search },
+                                                { replace: true },
+                                            )
+                                        }
+                                    />
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableFooter>
+                </Table>
+            </TableContainer>
         </BasePageLayout>
     );
 }
