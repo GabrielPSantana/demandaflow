d9cf1584b0fe36c8b9c1bfbb640bb5998ba50a89
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-17 22:41:31 -0300

    feat: altera importação de pagination e filteres no viewset

--- back-end/task-service/api/views/task_view_set.py
+++ back-end/task-service/api/views/task_view_set.py
@@ -1,7 +1,7 @@
 from abc import ABC
 from rest_framework.viewsets import GenericViewSet
-from .pagination import DefaultPagination
-from .filters import DefaultOrderingFilter
+from ..pagination import DefaultPagination
+from ..filters import DefaultOrderingFilter
 
 class TaskViewSet(ABC, GenericViewSet):
     pagination_class = DefaultPagination
