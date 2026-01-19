7280fb1140b32869d1bc9f36fd6b2d6486a11ba9
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-19 05:20:31 -0300

    feat: adiciona serialização dos dados na view de create

--- back-end/task-service/api/views/task_create_view_set.py
+++ back-end/task-service/api/views/task_create_view_set.py
@@ -10,27 +10,20 @@ from drf_yasg import openapi
 
 class TaskCreateViewSet(TaskViewSet):
     def create(self, request):
-        data_request = request.data
-
         user_id='d4f7c2a8-3e5b-4f1d-9b2a-6c8f1a2e7d9b'  
-        
-        if not user_id or not data_request.get('title'):
-            return Response(
-                {"error": "Campos inválidos."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
+    
+        serializer = TaskSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+    
         try:
             task = TaskCreateService().execute(
                 user_id=user_id,
-                title=data_request.get('title'),
-                description=data_request.get('description'),
-                team_id=data_request.get('team_id'),
-                priority=data_request.get('priority'),
-                status=data_request.get('status'),
-                start_datetime=data_request.get('start_datetime'),
-                end_datetime=data_request.get('end_datetime'),
-                time_spent=data_request.get('time_spent'),
+                title=serializer.data['title'],
+                description=serializer.data['description'],
+                priority=serializer.data['priority'],
+                status=serializer.data['status'],
+                start_datetime=serializer.data['start_datetime'],
+                end_datetime=serializer.data['end_datetime'],
             )
             
             return Response(TaskSerializer(task).data, status=status.HTTP_201_CREATED)
@@ -44,6 +37,6 @@ class TaskCreateViewSet(TaskViewSet):
         except Exception as error:
 
             return Response(
-                {"error": "Ocorreu um erro interno ao processar a requisição."},
+                {"error": f"Ocorreu um erro interno ao processar a requisição.{error}"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
