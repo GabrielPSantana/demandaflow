0a368035875513125083e468fbcebea7b0727b83
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-18 22:30:31 -0300

    feat: adiciona serialização de datas

--- back-end/task-service/api/serializers/task_serializer.py
+++ back-end/task-service/api/serializers/task_serializer.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
+from django.utils import timezone
 from ..models import Task
 
+
 class TaskSerializer(serializers.ModelSerializer):
+    start_datetime = serializers.DateTimeField(
+        format="%Y-%m-%dT%H:%M",
+        input_formats=["%Y-%m-%dT%H:%M"],
+        required=False,
+        allow_null=True
+    )
+
+    end_datetime = serializers.DateTimeField(
+        format="%Y-%m-%dT%H:%M",
+        input_formats=["%Y-%m-%dT%H:%M"],
+        required=False,
+        allow_null=True
+    )
+
     class Meta:
         model = Task
         fields = [
@@ -9,4 +25,3 @@ class TaskSerializer(serializers.ModelSerializer):
             'status', 'start_datetime', 'end_datetime',
             'time_spent', 'team_id'
         ]
- 
