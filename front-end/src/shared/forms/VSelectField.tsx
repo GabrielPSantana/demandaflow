9babfcc54e9b8e4bf4b166e0d3688d904bf4fd10
GabrielPSantana <gabrieldevti@gmail.com>
2026-01-18 22:24:56 -0300

    feat: adiciona input de seleção

---
+++ front-end/src/shared/forms/VSelectField.tsx
@@ -0,0 +1,60 @@
+import {
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    FormHelperText,
+    type SelectProps,
+} from '@mui/material';
+import { useField } from '@unform/core';
+import { useEffect, useState } from 'react';
+
+type SelectOption = {
+    value: string;
+    label: string;
+};
+
+type VSelectFieldProps = SelectProps & {
+    name: string;
+    label: string;
+    options: SelectOption[];
+};
+
+export const VSelectField = ({ name, label, options, ...rest }: VSelectFieldProps) => {
+    const { fieldName, registerField, defaultValue, error, clearError } = useField(name);
+
+    const [value, setValue] = useState(defaultValue ?? '');
+
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            getValue: () => value,
+            setValue: (_, newValue) => setValue(newValue),
+            clearValue: () => setValue(''),
+        });
+    }, [fieldName, registerField, value]);
+
+    return (
+        <FormControl fullWidth error={!!error}>
+            <InputLabel>{label}</InputLabel>
+
+            <Select
+                {...rest}
+                label={label}
+                value={value}
+                onChange={(e) => {
+                    clearError();
+                    setValue(e.target.value);
+                }}
+            >
+                {options.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </Select>
+
+            {error && <FormHelperText>{error}</FormHelperText>}
+        </FormControl>
+    );
+};
